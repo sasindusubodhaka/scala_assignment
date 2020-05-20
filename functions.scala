2a358761f78235dd7fe652a675563fe48dbf490e
Your Name <sasindusubodhaka@gmail.com>
2020-05-21 02:21:59 +0530

    I add some comments

--- functions.scala
+++ functions.scala
@@ -1,13 +1,16 @@
 object functions{
-	def ctof(x:Double):Double={
+	//function of converting celsius to fahrenheit
+	def ctof(x:Double):Double={ 
 		return x*1.8000 + 32.0000;
 	}
+	//function of calculating the volume of sphere
 	def volume(y:Double):Double={
 		var pi=22f/7;
 		var r=y*y*y;
 		return (4f/3)*pi*r;
 		
 	}
+	//function of  calculating the cost of given books
 	def cost(z:Double):Double={
 		var discounted_cost=(1-0.4)*24.95*z;
 		var shiping_cost=0.00;
