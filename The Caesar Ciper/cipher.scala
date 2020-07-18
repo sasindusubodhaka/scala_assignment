407e42871388056b797b4682f6a55649f71206cb
Your Name <sasindusubodhaka@gmail.com>
2020-07-18 21:50:13 +0530

    Index:18001564 Assignment:Caesar Ciper

--- The Caesar Ciper/cipher.scala
+++ The Caesar Ciper/cipher.scala
@@ -5,61 +5,60 @@ object cipher{
 	def main(args:Array[String]){
 
 	println("..............Caesar Cipher Method..............")
-			print("Enter the string: ");
-		    s=scala.io.StdIn.readLine();
-			print("Enter the the number you want to shif:");
-		    x=scala.io.StdIn.readInt();
-			
-            ciper(encript,decript)			//passing the encript function to ciper function		
+	print("Enter the string: ");
+	s=scala.io.StdIn.readLine();
+	print("Enter the key:");
+	x=scala.io.StdIn.readInt();
+	if(x>26) x=x%26			//check shifting number is greater than 26
+	ciper(encript,decript)		//passing the encript function to ciper function		
 
 	}
-    def ciper(e:(String,Int)=>ListBuffer[Char],d:(String,Int)=>ListBuffer[Char])={
-			if(x>26) x=x%26			//check shifting number is greater than 26		
-            var list=e(s,x)			//pass the inputed text and the shiftinf number to encript function
-         	print("Encripted String is: ")
-			list.foreach(j=>print(j));
-
+    def ciper(e:(String,Int)=>ListBuffer[Char],d:(String,Int)=>ListBuffer[Char])={			
+            var list=e(s,x)			//pass the inputed text and the key to encript function
+            print("Encripted String : ")
+	    list.foreach(j=>print(j));
+	    println(" ");
             var n=list.mkString
-			var declist=d(n,x);
-			print("Decripted String is: ")
-			declist.foreach(k=>print(k))
+	    var declist=d(n,x);
+	    print("Decripted String : ")
+	    declist.foreach(k=>print(k))
     }
 	def encript(s:String,x:Int):ListBuffer[Char]={
 		var i=0;
 		var l= new ListBuffer[Char]() 	//create a list to store encripted characters
 		while(i<s.size){
-			var n=s.charAt(i);			//select characters one by one in the string
-			var j=n+x;					//shifting the ASCII value of the character(encription)
+			var n=s.charAt(i);	//select characters one by one in the string
+			var j=n+x;		//shifting the ASCII value of the character(encription)
 			var c=0;
-			if(n.isUpper){				//check the character is uppercase or not
-					if(j>90) c=j-90+64 else c=j;	//check the limit of the shifted value
+			if(n.isUpper){		//check the character is uppercase or not
+				if(j>90) c=j-90+64 else c=j;	//check the limit of the shifted value
 					
-				}
+			}
 			else{
-					if(j>122) c=j-122+96 else c=j;   
+				if(j>122) c=j-122+96 else c=j;   
 										
 			}
-			l+=c.toChar					//store the encripted character in the list
+			l+=c.toChar			//store the encripted character in the list
 			i=i+1;
 		}
-		l								//return the list
+		l					//return the list
 	}
 	def decript(s:String,x:Int):ListBuffer[Char]={
 		var i=0;
 		var l= new ListBuffer[Char]()  //create a list to store decripted characters
 		while(i<s.size){
 			var n=s.charAt(i);			//select characters one by one in the string
-			var j=n-x;					//shifting the ASCII value of the character(decription)
+			var j=n-x				//shifting the ASCII value of the character(decription)                        
 			var c=0;
 			if(n.isUpper){				//check the character is uppercase or not
-					if(j<65) c=90-64-j else c=j;  //check the limit of the shifted value
+				if(j<65) c=90-(64-j) else c=j;  //check the limit of the shifted value
 			}
 			else{
-					if(j<97) c=122-96-j else c=j;				
+				if(j<97) c=122-(96-j) else c=j;				
 			}
-			l+=c.toChar					//store the decripted character in the list
+			l+=c.toChar				//store the decripted character in the list
 			i=i+1;
 		}
-		l								//return the list					
+		l	      					//return the list					
 	}
 }
